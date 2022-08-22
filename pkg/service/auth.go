2d1d74dd2e32c77cc765128c25fc6a64d29234dc
roxyash <roxyash@mail.ru>
2022-08-22 18:50:50 +0300

    create methods generateJwtToken/parseJwtToken/Addjwttokentoheader for use internal methods

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -2,13 +2,25 @@ package service
 
 import (
 	"crypto/sha1"
+	"errors"
 	"fmt"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	todo "github.com/roxyash/todolist"
 	"github.com/roxyash/todolist/pkg/repository"
 )
 
-const salt = "qwertyuiop[asdfghjkl123cvbnm,"
+const (
+	salt       = "qwertyuiop[asdfghjkl123cvbnm,"
+	singingKey = "qweasdjoiasjdoi@asd#$2a"
+	tokenTTL   = 12 * time.Hour
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -20,8 +32,46 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
-	
+
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(singingKey))
+}
+
+func (s *AuthService) ParseToken(accessToken string) (int, error) {
+	token, err := jwt.ParseWithClaims(accessToken, &tokenClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+
+		return []byte(singingKey), nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	claims, ok := token.Claims.(*tokenClaims)
+	if !ok {
+		return 0, errors.New("token claims are not of type *tokenClaims")
+	}
+
+	return claims.UserId, nil
 }
 
 func generatePasswordHash(password string) string {
