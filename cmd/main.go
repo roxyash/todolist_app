49def96f19b899174a6be55944555aa539ddc0c8
roxyash <roxyash@mail.ru>
2022-08-22 18:20:34 +0300

    add method create user for handler sign-up

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -10,16 +9,19 @@ import (
 	"github.com/roxyash/todolist/pkg/handler"
 	"github.com/roxyash/todolist/pkg/repository"
 	"github.com/roxyash/todolist/pkg/service"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
 
 func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+	
 	if err := initConfig(); err != nil {
-		log.Fatalf("error initializing configs: %s", err.Error())
+		logrus.Fatalf("error initializing configs: %s", err.Error())
 	}
 
 	if err := godotenv.Load(); err != nil {
-		log.Fatalf("error loading env variables: %s", err.Error())
+		logrus.Fatalf("error loading env variables: %s", err.Error())
 	}
 	db, err := repository.NewPostgresDB(repository.Config{
 		Host:     viper.GetString("db.host"),
@@ -31,7 +33,7 @@ func main() {
 	})
 
 	if err != nil {
-		log.Fatalf("failed to initialize db: %s", err.Error())
+		logrus.Fatalf("failed to initialize db: %s", err.Error())
 	}
 
 	repos := repository.NewRepository(db)
@@ -40,7 +42,7 @@ func main() {
 
 	srv := new(todo.Server)
 	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		log.Fatalf("error ocered while running http server: %s", err.Error())
+		logrus.Fatalf("error ocered while running http server: %s", err.Error())
 	}
 }
 
--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -6,6 +6,14 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
+const (
+	usersTable       = "users"
+	todoListsTable   = "todo_lists"
+	usersListsTable  = "users_lists"
+	todoItemsTable   = "todo_items"
+	listsItemsTables = "lists_items"
+)
+
 type Config struct {
 	Host     string
 	Port     string
@@ -18,14 +26,14 @@ type Config struct {
 func NewPostgresDB(cfg Config) (*sqlx.DB, error) {
 	db, err := sqlx.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
 		cfg.Host, cfg.Port, cfg.Username, cfg.DBName, cfg.Password, cfg.SSLMode))
-	
+
 	if err != nil {
 		return nil, err
 	}
 
 	err = db.Ping()
 	if err != nil {
-		return nil, err 
+		return nil, err
 	}
 
 	return db, nil
